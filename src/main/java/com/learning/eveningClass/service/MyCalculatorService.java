4f843525c267958418dda37c3e29fa7a865cd8e1
mwizerwa77 <mwizerwa77@gmail.com>
2022-10-16 14:44:19 +0200

    initial commit

---
+++ src/main/java/com/learning/eveningClass/service/MyCalculatorService.java
@@ -0,0 +1,11 @@
+package com.learning.eveningClass.service;
+
+public interface MyCalculatorService {
+
+	int add(int[] data);
+	
+	int subract(int num1, int num2);
+	
+	int multiply(int[] data);
+	
+}
